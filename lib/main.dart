276f393067895b576f7ea8082bd5ebb79c3e3370
jokerredsoil <septianxnugraha@gmail.com>
2024-12-14 23:34:18 +0700

    fix code adding asset .env to yaml

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text('TMDB App')),
         body: Center(child: Text('Flutter DotEnv Example')),
-      ),
+      ), 
     );
   }
 }
--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -7,6 +7,9 @@ class ApiClient {
 
   String get _apiKey => dotenv.env['API_KEY'] ?? '';
 
+
+
+
   Future<http.Response> get(String endpoint) async {
     final url = '$_baseUrl$endpoint&api_key=$_apiKey';
     return await http.get(Uri.parse(url));
